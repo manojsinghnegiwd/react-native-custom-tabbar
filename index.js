41af42fb59c4ac22f70f7a5a75274000a26fda95
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-22 23:32:34 +0530

    added TabBar component

---
+++ index.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
+
+import Button from './button';
+
+export default class TabBar extends Component {
+  render () {
+
+    let {buttons, buttonTextColor, tabStyle, tabActiveStyle, activeTab, rippleColor} = this.props;
+
+		let newStyle = [styles.tabs];
+
+		if(tabStyle) {
+			newStyle = newStyle.concat(tabStyle);
+		}
+
+		if(!tabActiveStyle) {
+			tabActiveStyle = [];
+		}
+
+    return (
+      <View style={{alignSelf: "stretch", height: 60, flexDirection: "row"}}>
+        {buttons && buttons.length != 0 ? buttons.map((button, index) => {
+          return (
+            <View key={index} style={{flex:1}}>
+              <Button isRipple onPress={button.onPress} style={newStyle.concat( activeTab == index ? tabActiveStyle : [] )} rippleColor={rippleColor}>
+                <Text style={{color: buttonTextColor}}> {button.text} </Text>
+              </Button>
+            </View>
+          )
+        }) : null}
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  tabs: {
+		justifyContent: "center",
+		alignItems: "center",
+		height: 60
+  }
+})
